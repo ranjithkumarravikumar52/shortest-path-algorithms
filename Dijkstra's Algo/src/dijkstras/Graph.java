d3b03468e10b89c2edd3e4a1f0fc28968a1bc4d3
Ranjith <ranjithkumarravikumar52@gmail.com>
2018-05-10 16:13:54 -0400

    Major updates to starter code

--- Dijkstra's Algo/src/dijkstras/Graph.java
+++ Dijkstra's Algo/src/dijkstras/Graph.java
@@ -1,17 +1,26 @@
 package dijkstras;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Graph {
 
     private List<Vertex> verticesInTheGraph;
 
+    public Graph() {
+        this.verticesInTheGraph = new ArrayList<>();
+    }
+    
     public List<Vertex> getVerticesInTheGraph() {
         return verticesInTheGraph;
     }
 
     public void setVerticesInTheGraph(List<Vertex> verticesInTheGraph) {
         this.verticesInTheGraph = verticesInTheGraph;
+    }
+    
+    public void addVerticesToTheGraph(Vertex newVertex){
+        this.verticesInTheGraph.add(newVertex);
     }
     
     public void findShortestPath(Vertex sourceVertex){
